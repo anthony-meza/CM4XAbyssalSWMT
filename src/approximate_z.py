78801f224fac64c38fbbda2b6bf8bcfe008ca537
Anthony Meza <ameza98@outlook.com>
2026-04-21 16:37:48 -0400

    updating notebook

--- src/approximate_z.py
+++ src/approximate_z.py
@@ -1,34 +1,37 @@
 import xarray as xr 
 import numpy as np 
 import pandas as pd
-def approximate_z_on_boundaries_top_down(ds, dim = "sigma2", zos = None):
+
+def approximate_z_on_boundaries_top_down(ds, dim="sigma2", zos=None):
     thicknesses = ds.thkcello.fillna(0.0)
 
-    if zos is None: 
-        zos = ds.zos
-    eta = (0.0 * thicknesses.isel({f"{dim}_l":0})) + zos
+    if zos is None:
+        if "zos" in ds:
+            zos = ds["zos"]
+        else:
+            zos = thicknesses.sum(f"{dim}_l") - ds.deptho
+
+    eta = (0.0 * thicknesses.isel({f"{dim}_l": 0})) + zos
     eta.coords[f"{dim}_l"] = -100
 
-    #calculate the vertical position of cell boundaries using 
-    #cumsum
-    cell_boundaries = xr.concat([eta, (-thicknesses)], dim = f"{dim}_l")
-    cell_boundaries = cell_boundaries.cumsum(dim = f"{dim}_l")
-    cell_boundaries = cell_boundaries.rename({f"{dim}_l":f"{dim}_i"})
-
+    cell_boundaries = xr.concat([eta, -thicknesses], dim=f"{dim}_l")
+    cell_boundaries = cell_boundaries.cumsum(dim=f"{dim}_l")
+    cell_boundaries = cell_boundaries.rename({f"{dim}_l": f"{dim}_i"})
     cell_boundaries.coords[f"{dim}_i"] = ds.coords[f"{dim}_i"]
-    return cell_boundaries.where(ds.wet > 0) #needed for some reason, can't apply to 
-        
-def approximate_z_top_down(ds, dim = "sigma2", zos = None):    
-    cell_boundary_height = approximate_z_on_boundaries_top_down(ds, dim = dim, zos = zos)
+
+    return cell_boundaries.where(ds.wet > 0)
+
+def approximate_z_top_down(ds, dim="sigma2", zos=None):
+    cell_boundary_height = approximate_z_on_boundaries_top_down(ds, dim=dim, zos=zos)
 
     h_n = cell_boundary_height.isel({f"{dim}_i": slice(0, -1)})
     h_np1 = cell_boundary_height.isel({f"{dim}_i": slice(1, None)})
     h_np1.coords[f"{dim}_i"] = h_n.coords[f"{dim}_i"]
 
-    cell_center_height = (h_np1 + h_n) / 2
-
-    cell_center_height = cell_center_height.rename({f"{dim}_i":f"{dim}_l"})
+    cell_center_height = 0.5 * (h_np1 + h_n)
+    cell_center_height = cell_center_height.rename({f"{dim}_i": f"{dim}_l"})
     cell_center_height.coords[f"{dim}_l"] = ds.coords[f"{dim}_l"]
+
     return cell_center_height.where(ds.wet > 0)
     
 def approximate_z_on_boundaries_bottom_up(ds, dim = "sigma2"):
